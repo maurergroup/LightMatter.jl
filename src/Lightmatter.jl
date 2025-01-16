9ea23589061c4f97dc04dcffb661ac4d1751d642
HenrySnowden <snowden.ht@gmail.com>
2025-01-16 16:08:17 +0000

    Removed unnessary depedencies

--- src/Lightmatter.jl
+++ src/Lightmatter.jl
@@ -1,6 +1,6 @@
 module Lightmatter
 
-using DataInterpolations, DelimitedFiles, Integrals, Roots, RecursiveArrayTools, OrdinaryDiffEq, HDF5, FastGaussQuadrature, Trapz, Interpolations, JLD2, Unitful, LinearAlgebra
+using DataInterpolations, DelimitedFiles, Integrals, Roots, RecursiveArrayTools, OrdinaryDiffEq, HDF5, Interpolations, LinearAlgebra
 
 export define_laser_system, define_simulation_settings, define_material_parameters, Constants, define_sim_dimensions, function_builder, run_simulation, post_production, get_volume
 
