4618a5428ecceeb91828b02f6d4b0bdfa79d8797
Snowd1n <snowden.ht@gmail.com>
2025-06-11 11:07:48 +0100

    Added forwarddiff to dependencies

--- src/Lightmatter.jl
+++ src/Lightmatter.jl
@@ -1,6 +1,6 @@
 module Lightmatter
 
-using DataInterpolations, DelimitedFiles, Integrals, NonlinearSolve, RecursiveArrayTools, OrdinaryDiffEq, HDF5, LinearAlgebra, GeneralizedGenerated, Unitful, JLD2
+using DataInterpolations, DelimitedFiles, Integrals, NonlinearSolve, RecursiveArrayTools, OrdinaryDiffEq, HDF5, LinearAlgebra, GeneralizedGenerated, Unitful, JLD2, ForwardDiff
 
 export build_Simulation, run_simulation, post_production, Constants, DensityMatrix, build_DensityMatrix, build_Dimension, FE_initialization
 export ElectronicTemperature, build_ElectronicTemperature, PhononicTemperature, build_PhononicTemperature, function_builder
